ed391bdd88cd96306035deb303e95a0a2def6d5a
Tanishqua24 <91310415+Tanishqua24@users.noreply.github.com>
2022-01-02 20:53:05 +0530

    Add files via upload

---
+++ binarysearch.java
@@ -0,0 +1,30 @@
+class Solution
+{
+    public int binarysearch(int[] nums, int target)
+    {
+        int low =0;
+        int high=nums.length -1;
+        int mid=0;
+        
+        while(low <= high)
+        {
+          
+            if(target==nums[mid])
+            {
+                return mid;
+            }
+  mid = high+(low-high)/2;
+            
+            if(target > nums[mid])
+            {
+                low = mid + 1;
+            }
+            else if(target < nums[mid])
+            {
+                high = mid - 1; 
+            } 
+            
+        }
+        return -1;
+    }
+} 
